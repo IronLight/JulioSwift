429016c6561a01d2613ed399e8805f05ab4821d3
IronLight <alexandre.sechi@gmail.com>
2016-03-13 16:49:16 +0100

    Score comment

--- Swift-Project/ThoufffController.swift
+++ Swift-Project/ThoufffController.swift
@@ -15,6 +15,7 @@ class ThoufffController: UIViewController {
         super.viewDidLoad()
         // Do view setup here.
         
+        // Ici on essais d'avoir un score assez grand, plus le temps passe et moins le score sera haut. (Max 60 000)
         let TimerScore: Double = (30 - Score.Static.myScore) * 1000
         
         TheScore.text = "Ton score est : \(String(format: "%.0f", TimerScore))"
